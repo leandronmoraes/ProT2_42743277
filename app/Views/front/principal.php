95156a8464408dae46d9f62d9e967972d4b51e42
Moraes Leandro <moraesleandro1228@gmail.com>
2025-05-14 15:53:00 -0300

    Primer commit tramo 2 - se incorporo boostrap, codeigniter - y correspondientes configuraciones

---
+++ app/Views/front/principal.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Página Principal</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet" >
+  </head>
+  <body>
+    <h1>Página CódigoRoto</h1>
+    <script src="assets/js/bootstrap.bundle.min.js" ></script>
+  </body>
+</html>
